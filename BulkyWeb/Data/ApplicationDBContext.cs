feefdd028bc1e5fe40b776b4a389f567faed1f18
global8696 <8696global@gmail.com>
2024-03-09 19:54:50 +0530

    Udemy Course changes

---
+++ BulkyWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+	public class ApplicationDBContext : DbContext
+
+	{
+		public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+		{
+
+		}
+	}
+}
+
